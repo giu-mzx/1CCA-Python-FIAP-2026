87b3c35485d79fa192c7986fc832a5d73bc0f4a7
giu-mzx <giu.brasolin@gmail.com>
2026-03-20 11:31:36 -0300

    aula 02 variáveis e operadores

---
+++ aula02-variaveis-operadores/aula02a.py
@@ -0,0 +1,29 @@
+print("Olá mundo")
+
+print(7+4)
+print("7+4")
+print("7" + "4") # Concatenação de Strings
+
+# Comentário de 1 linha
+'''
+Comentários de 
+múltiplas 
+linhas
+'''
+
+# Variáveis
+nome = "Alexandre" # str
+idade = 26 # int
+peso = 70.2 # float
+
+print(nome, idade, peso)
+print(f"Oiiii {nome}!!!!")
+
+# Input - Simulação de um forms mp CMD
+nome = input("Digite o seu nome: ")
+idade = int(input("Digite a sua idade: "))
+
+nova_idade = idade + 1
+
+print(nome, idade)
+print(nova_idade)
---
+++ aula02-variaveis-operadores/desafios_aula02.py
@@ -0,0 +1,17 @@
+# Desafio 01
+
+nome = input("Digite seu nome: ")
+print(f"Boas-vindas, {nome}!!")
+
+# Desafio 02
+
+dia = input("Insira o dia do seu nascimento: ")
+mês = input("Insira o mês do seu nascimento: ")
+ano = input("Insira o ano do seu nascimento: ")
+print()
+
+# Desafio 03
+
+n01 = int(input("Insira o primeiro número: "))
+n02 = int(input("Insira o segundo número: "))
+print(n01 + n02)
